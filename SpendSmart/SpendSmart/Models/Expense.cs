bc8bdc2b2ee18635fb464b61aa0e3d240b814f36
ulvirasulow <ulvirasul1@gmail.com>
2024-12-10 03:53:00 +0400

    finished

---
+++ SpendSmart/SpendSmart/Models/Expense.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SpendSmart.Models
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+        public double Value { get; set; }
+        [Required]
+        public string? Description { get; set; }
+    }
+}
